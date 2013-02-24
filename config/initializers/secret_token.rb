20deebe27609fc813ca179eb3124a76042ea15f7
Nathan Butler <nbutler48@gmail.com>
2013-02-24 15:11:32 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hue::Application.config.secret_token = 'bf0e0b7822a2b4dc360f5ba17a48253fc810204d41551c1abe921de33463e8ff9c64296b3b494dce18b629e222e3ebb3a4a8770d3e3a3904dfcdb4e26c7139e6'
